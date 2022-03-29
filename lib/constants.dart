385216b1ca8aa9f7f92621081596dcce1134d54b
Shashank Rewankar <shasha98sr@gmail.com>
2022-03-29 21:31:19 +0530

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+
+
+import 'dart:ui';
+
+const defaultPadding = 20.0;
+
+const primaryColor = Color(0xFF48c4f7);
+const secondaryColor = Color(0xFF42b1dc);
+const backgroundColor = Color(0xFFaee7fa);
+const lightGrey = Color(0xFF6c6c6c);
